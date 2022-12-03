2a3d40937a696548005e6c9d558d7dbce4f372cc
Olga Ukhanova <uhole4ka@rambler.ru>
2022-12-03 23:29:28 +0300

    Добавили решение задач 41, 43

---
+++ Z41/Program.cs
@@ -0,0 +1,97 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write("Введите числа через запятую: ");
+string a =ReadDigitsFromConsole();
+int count=0;
+int j=0;
+int[] arr=new int[IndexComa(a)];
+char c=',';
+string s = "";
+
+for (int i=0; i<a.Length;i++)
+{
+    if (a[i]==c)
+    {arr[j++]=Convert.ToInt32(s);
+    s="";}
+    else {s+=a[i];}
+}
+if (arr[j]!=c)
+ {arr[j]=Convert.ToInt32(s);}
+ PrintArray(arr);  
+
+ for (int i=0; i<arr.Length; i++) 
+ {
+    if (arr[i]<0)
+    {count++;}
+ }
+ Console.Write(" -> " + count);   
+if (count==0) {Console.Write(" , Все числа положительные");}
+
+int IndexComa(string s)
+{   int countsymbol=1;
+    for (int i=0; i<s.Length;i++)
+    if(s[i]==',')
+    {
+      countsymbol++;  
+    }
+    return countsymbol;
+}
+
+string ReadDigitsFromConsole()
+        {
+            string result = "";
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Backspace:
+                        if (result.Length > 0)
+                        {
+                            result = result.Remove(result.Length - 1, 1);
+                            Console.Write(key.KeyChar + " " + key.KeyChar);
+                        }
+                        break;
+
+                    case ConsoleKey.Enter:
+                        Console.WriteLine();
+                        return result;
+
+                    case ConsoleKey.OemComma:
+
+                        if (result[result.Length-1] == ',')
+                        {Console.Write("\b"+ key.KeyChar);}
+                        else {Console.Write(key.KeyChar);
+                        result += key.KeyChar;}
+                        break;
+                    case ConsoleKey.OemMinus:
+
+                        if (result.Length>0 && (result[result.Length-1] == '-' || char.IsDigit(result[result.Length-1])))
+                        {Console.Write("\b"+result[result.Length-1]);}
+                        else {Console.Write(key.KeyChar);
+                        result += key.KeyChar;}
+                        break;
+
+                    default:
+                        if (char.IsDigit(key.KeyChar) || key.KeyChar == '-' || key.KeyChar == ',')
+                        {
+                            {Console.Write(key.KeyChar);
+                            result += key.KeyChar;
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i=0;i<count;i++)
+    {
+        Console.Write(array[i] + "|");
+    }
+}
---
+++ Z43/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 43: Напишите программу, которая найдёт 
+точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+double b1 = ReadInt("Введите b1: ");
+double k1 = ReadInt("Введите k1: ");
+double b2 = ReadInt("Введите b2: ");
+double k2 = ReadInt("Введите k2: ");
+double x=0;
+double y=0;
+
+if (k1!=k2)
+{
+   x=(b2-b1)/(k1-k2);
+   y=k1*x+b1; 
+   Console.WriteLine($" -> ( {x} ; {y})");
+}
+else if (b1!=b2)
+{Console.WriteLine("Прямые параллельны");}
+else {Console.WriteLine("Прямые накладываются друг на друга");}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
